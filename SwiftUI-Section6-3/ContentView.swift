eaa9189a63f77a26bb47a0d195537a6753c507f3
lioisshi <84262564+lioisshi@users.noreply.github.com>
2021-06-12 12:04:55 +0900

    モーダル

--- SwiftUI-Section6-3/ContentView.swift
+++ SwiftUI-Section6-3/ContentView.swift
@@ -6,16 +6,44 @@
 //
 
 import SwiftUI
-
+//p303
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+    //Modalが開いているかを管理する変数
+    @State var isModal: Bool = false
+    @State var counter: Int = 0
+    var body: some View{
+        Button(action: {
+            isModal = true
+        }){
+            Text("Sheet テスト")
+        }.sheet(isPresented: $isModal, onDismiss: {countUp()}){
+            SomeView()
+        }.disabled(counter >= 3)
+        Text("回数: \(counter)")
+            .font(.title)
             .padding()
     }
+    func countUp(){
+        counter += 1
+    }
 }
+struct SomeView :View{
+    var body :some View{
+        VStack{
+            Text("プレゼンテーション")
+            //p201
+            Image(systemName: "gift")
+                .imageScale(.large)
+                .padding()
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        SomeView()
     }
 }
+
